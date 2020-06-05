9486cdcda50693a3b9e38971786bc84d842e8ad6
unknown <gayat19>
2020-06-05 10:17:11 +0530

    First Commit

---
+++ src/java_day9/Customer.java
@@ -0,0 +1,27 @@
+package java_day9;
+
+public class Customer {
+
+	private int id,age;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
